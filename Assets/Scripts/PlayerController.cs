276457920ae7c68f138fc1354f9a05a4c9647b1c
Anton Kulikov <800054074@omnia.fi>
2024-10-15 11:07:03 +0300

    Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 3f;            
+    public Rigidbody2D rb;                  
+
+    private Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+            
     }
 
     // Update is called once per frame
     void Update()
     {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");    
+
         
+        movement = new Vector2(moveX, moveY).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movement * moveSpeed;
     }
 }
